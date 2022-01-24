b30bc0f75d777d4f2332d41d62a4053efd4c8974
Nicolas Burbinski <nburbinski@gmail.com>
2022-01-23 20:25:40 -0500

    initial setup

---
+++ src/components/NavBar.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const NavBar = () => {
+  return (
+    <span>
+      <p>How to play</p>
+      <h1>WORDLE</h1>
+      <p>Stats</p>
+      <p>Settings</p>
+    </span>
+  );
+};
+
+export default NavBar;
