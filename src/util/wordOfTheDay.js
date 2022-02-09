8dd1420437a697f4a00aa7bc3affd1e6308cbeb7
Nicolas Burbinski <nburbinski@gmail.com>
2022-02-08 20:02:56 -0500

    date taken from parameter

--- src/util/wordOfTheDay.js
+++ src/util/wordOfTheDay.js
@@ -1,10 +1,9 @@
 import { ACTUALWORDS } from "../const/ACTUALWORDS";
 
-export function getWordOfDay() {
+export function getWordOfDay(date = new Date()) {
   const oneDay = 1000 * 60 * 60 * 24;
   const firstDay = new Date("June 19, 2021 00:00:00");
-  const now = new Date();
-  const index = Math.floor((now.getTime() - firstDay.getTime()) / oneDay);
+  const index = Math.floor((date.getTime() - firstDay.getTime()) / oneDay);
 
   return ACTUALWORDS[index].toUpperCase();
 }
